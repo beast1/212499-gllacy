c6a6ae45adfce01c11fcf73d1ae8d201c52fd746
Егор <beast.loby@gmail.com>
2017-02-19 14:44:13 +0200

    Правки

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var btnFbOpen = document.querySelector(".contacts .btn");
+var modalFb = document.querySelector(".modal-feedback-wrapper");
+var btnFbClose1 = document.querySelector(".modal-feedback .modal-exit");
+//var modalFbContent = document.querySelector(".modal-feedback");
+
+btnFbOpen.addEventListener("click", function(event) {
+    event.preventDefault();
+    modalFb.classList.add("visible");
+});
+
+btnFbClose1.addEventListener("click", function(event) {
+    event.preventDefault();
+    modalFb.classList.remove("visible");
+});
+
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+        if (modalFb.classList.contains("visible")) {
+            event.preventDefault();
+            modalFb.classList.remove("visible");
+        }
+    }
+});
+
+//modalFbContent.addEventListener("click", function(event) {
+//    event.preventDefault();
+//    modalFb.classList.add("hidden");
+//});
