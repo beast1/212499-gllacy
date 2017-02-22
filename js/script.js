5825a2127801c7c077b043c5f4c10edf939ec42a
Егор <beast.loby@gmail.com>
2017-02-22 17:14:05 +0200

    Пост 7

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 var btnFbOpen = document.querySelector(".contacts .btn");
-var modalFb = document.querySelector(".modal-feedback-wrapper");
+var modalFbW = document.querySelector(".modal-feedback-wrapper");
+var modalFb = document.querySelector(".modal-feedback");
 var btnFbClose1 = document.querySelector(".modal-feedback .modal-exit");
 
 btnFbOpen.addEventListener("click", function(event) {
     event.preventDefault();
-    modalFb.classList.add("modal-feedback-visible");
+    modalFbW.classList.add("visible");
+    modalFb.classList.add("ajs-modal-feedback");
 });
 
 btnFbClose1.addEventListener("click", function(event) {
     event.preventDefault();
-    modalFb.classList.remove("modal-feedback-visible");
+    modalFbW.classList.remove("visible");
+    modalFb.classList.remove("ajs-modal-feedback");
 });
 
 window.addEventListener("keydown", function(event) {
     if (event.keyCode === 27) {
-        if (modalFb.classList.contains("modal-feedback-visible")) {
+        if (modalFbW.classList.contains("visible")) {
             event.preventDefault();
-            modalFb.classList.remove("modal-feedback-visible");
+            modalFbW.classList.remove("visible");
+            modalFb.classList.remove("ajs-modal-feedback");
         }
     }
 });
